b7d7b8a2f58a470e14ebdfb19a6fd8b0d10bdc0f
Nitin Goyal <inbox.nitin.goyal@gmail.com>
2025-01-20 17:24:29 +0530

    tweak: remove csvBox

--- src/view/settings/index.js
+++ src/view/settings/index.js
@@ -1,55 +1,12 @@
 import React from "react";
-import axios from "axios";
-import { CSVBoxButton } from "@csvbox/react";
 
 const Settings = () => {
-  const getData = async () => {
-    const options = {
-      method: "GET",
-      url: "http://localhost:5000/getData",
-      params: {},
-    };
-
-    const resData = await axios.request(options);
-    console.log("🚀 ~ file: index.js:10 ~ getData ~ resData:", resData);
-  };
-
   return (
     <div className="h-full w-full">
       <div className="text-left font-medium	">Organization</div>
-      {/* <div className="cursor-pointer mt-4" onClick={getData}>
+      <div className="cursor-pointer mt-4">
           Coming Soon...
-        </div> */}
-      <CSVBoxButton
-        licenseKey="WMCj7R9oAqMqfgIeUpEtgUQOVbJUL9"
-        user={{
-          user_id: "default123",
-        }}
-        onImport={(result, data) => {
-          if (result) {
-            console.log("success");
-            console.log(data.row_success + " rows uploaded");
-            console.log("🚀 ~ Settings ~ data:", data);
-            //custom code
-          } else {
-            console.log("fail");
-            //custom code
-          }
-        }}
-        render={(launch, isLoading) => {
-          return (
-            <button
-              className="p-2 px-4 rounded bg-slate-300"
-              disabled={isLoading}
-              onClick={launch}
-            >
-              Upload file
-            </button>
-          );
-        }}
-      >
-        Import
-      </CSVBoxButton>
+        </div>
     </div>
   );
 };
